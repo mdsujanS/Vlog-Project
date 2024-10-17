2a2436e78cfa54a6e0f0e1ea031f907f2048c551
Md Sujan Mia <mdsujanmia860@gmail.com>
2024-10-17 22:50:03 +0600

    add author user and user profile

--- profiles/views.py
+++ profiles/views.py
@@ -1,5 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .import forms 
 
 # Create your views here.
 def add_profile(request):
-    pass
+    if request.method == 'POST':
+        profile_form = forms.ProfileForm(request.POST)
+        if profile_form.is_valid():
+            profile_form.save()
+            return redirect('add_profile')
+        
+    else:
+        profile_form = forms.ProfileForm()
+    return render(request, 'profile/add_profile.html', {'form': profile_form})
